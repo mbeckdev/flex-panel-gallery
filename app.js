c7555ea8fcac635eb9ff3426050b688416e8cede
mbeckdev <mbeckdev42@gmail.com>
2021-08-29 13:10:58 -0600

    Add words flying in

--- app.js
+++ app.js
@@ -6,4 +6,16 @@ function toggleOpen() {
   this.classList.toggle('open');
 }
 
+function toggleActive(e) {
+  console.log(e.propertyName);
+  // if(e.propertyName === 'flex-grow')
+  // we would use flex   or flex-grow but some browsers use different ones so:
+  if (e.propertyName.includes('flex')) {
+    this.classList.toggle('open-active');
+  }
+}
+
 panels.forEach((panel) => panel.addEventListener('click', toggleOpen));
+panels.forEach((panel) =>
+  panel.addEventListener('transitionend', toggleActive)
+);
